4c33733df4a1eaed33fc530263171611c81dd0c2
feederbox826 <feederbox826@users.noreply.github.com>
2024-05-24 22:15:28 -0400

    rename files

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 # load schedules
 export DIFF_CRON=${DIFF_CRON:-"0 0 * * *"}
-echo "$DIFF_CRON cd /app && /app/backup.sh" >> /etc/crontabs/root
+echo "$DIFF_CRON cd /app && /app/backup-db.sh" >> /etc/crontabs/root
 
 export FULL_CROM=${FULL_CROM:-"0 0 */7 * *"}
-echo "$FULL_CROM cd /app && /app/backup.sh full" >> /etc/crontabs/root
+echo "$FULL_CROM cd /app && /app/backup-db.sh full" >> /etc/crontabs/root
 
 export CONSOLIDATE_CRON=${CONSOLIDATE_CRON:-"0 0 2 * *"}
-echo "$CONSOLIDATE_CRON cd /app && /app/consolidate.sh" >> /etc/crontabs/root
+echo "$CONSOLIDATE_CRON cd /app && /app/consolidate-db.sh" >> /etc/crontabs/root
 
 # run backup
 /app/backup.sh && crond -f
