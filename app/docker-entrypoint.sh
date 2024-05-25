2bf676dc8c2fea23a862a5dfa38b96e01fa77a97
feederbox826 <feederbox826@users.noreply.github.com>
2024-05-25 16:16:43 -0400

    fix entrypoint

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -10,5 +10,10 @@ echo "$FULL_CROM cd /app && /app/backup-db.sh full" >> /etc/crontabs/root
 export CONSOLIDATE_CRON=${CONSOLIDATE_CRON:-"0 0 2 * *"}
 echo "$CONSOLIDATE_CRON cd /app && /app/consolidate-db.sh" >> /etc/crontabs/root
 
+export IMG_CRON=${IMG_CRON:-"0 0 1 * *"}
+echo "$IMG_CRON cd /app && /app/img-backup.sh" >> /etc/crontabs/root
+
 # run backup
-/app/backup.sh && crond -f
+/app/backup-db.sh && \
+    /app/img-backup.sh &&\
+    crond -f
